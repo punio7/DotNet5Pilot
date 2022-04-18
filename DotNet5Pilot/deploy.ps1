93beda4b26606484133a673f2131c64dd26fc976
punio7 <punio7@gmail.com>
2022-04-18 10:06:21 +0200

    Fixed publish and deploy script

--- DotNet5Pilot/deploy.ps1
+++ DotNet5Pilot/deploy.ps1
@@ -1,4 +1,4 @@
 dotnet publish -c Release
 ssh pi@raspberrypi sudo systemctl stop pilot
-scp -r bin/Release/net5.0/publish/* pi@raspberrypi:/home/pi/pilot
+scp -r bin/Release/net6.0/publish/* pi@raspberrypi:/home/pi/pilot
 ssh pi@raspberrypi sudo systemctl start pilot
